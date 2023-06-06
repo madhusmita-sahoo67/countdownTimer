47d7209097e6676e0af764971e031b0742d8ed1a
MADHUSMITA SAHOO <94704599+madhusmita-sahoo67@users.noreply.github.com>
2023-06-06 20:38:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const BirthDay = "22 August 2023";
+
+function countdown() {
+    const bithDayDate = new Date(BirthDay);
+    const currentDate = new Date();
+
+    const totalSeconds = (bithDayDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
